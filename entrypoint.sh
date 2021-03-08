af1340018143212f1ef6ffcbd610c18278e3fd00
Philipp Schmitt <philipp@schmitt.co>
2021-03-08 21:47:59 +0100

    Fix for calendar names containing spaces

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env sh
 
-chown -R 1099:1099 /config
+if [ -n "$DEBUG" ]
+then
+  set -x
+fi
+
+CREDENTIALS_PATH="${CREDENTIALS_PATH:-/config/credentials.json}"
+TOKEN_PATH="${TOKEN_PATH:-/config/token}"
+CALENDAR="${CALENDAR}"
+ICS_URL="${ICS_URL}"
+
+GCAL_UID="$(id -u gcal)"
+GCAL_GID="$(id -g gcal)"
+chown -R "${GCAL_UID}:${GCAL_GID}" /config
 
 # Oneshot
 if [ -z "$INTERVAL" ]
 then
-  exec su gcal -c "/app/gcal_import.py -c '/config/credentials.json' -t '/config/token' $*"
+  exec su gcal -c "/app/gcal_import.py -c '${CREDENTIALS_PATH}' -t '${TOKEN_PATH}' $@"
 fi
 
 # Periodic import
 while true
 do
-  su gcal -c "/app/gcal_import.py -c '/config/credentials.json' -t '/config/token' $*"
+  su gcal -c "/app/gcal_import.py -c '${CREDENTIALS_PATH}' -t '${TOKEN_PATH}' $@"
 
   echo "Sleeping for $INTERVAL"
   sleep "$INTERVAL"
